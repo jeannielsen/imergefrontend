9a5a79aaf107334b784d86bc15b394a05ec46a61
Nielsen <jeanielsen@comcast.net>
2019-05-09 15:03:27 -0700

    Updates

---
+++ precache-manifest.7916bbe4621aef5c932dc866e24f6b27.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "533e1f1e2b8989f87776d5a0a992daf3",
+    "url": "/imergefrontend/index.html"
+  },
+  {
+    "revision": "9cccd148c2ded961646a",
+    "url": "/imergefrontend/static/css/main.6c291260.chunk.css"
+  },
+  {
+    "revision": "8597089ba7ba94ce64fa",
+    "url": "/imergefrontend/static/js/2.b71e12f8.chunk.js"
+  },
+  {
+    "revision": "9cccd148c2ded961646a",
+    "url": "/imergefrontend/static/js/main.53a4a950.chunk.js"
+  },
+  {
+    "revision": "8017c2b5250916e9603a",
+    "url": "/imergefrontend/static/js/runtime~main.32ae2932.js"
+  },
+  {
+    "revision": "e61807dc27ea8ce55807cda8c1f72169",
+    "url": "/imergefrontend/static/media/Restaurant.e61807dc.jpg"
+  },
+  {
+    "revision": "502d17b017bb372b563e764616119aa3",
+    "url": "/imergefrontend/static/media/airport-icon.502d17b0.png"
+  },
+  {
+    "revision": "1f3a13a3e50f20194deab78ca0b62aad",
+    "url": "/imergefrontend/static/media/bank-icon.1f3a13a3.png"
+  },
+  {
+    "revision": "20092106728b835ee2ebd0be047718aa",
+    "url": "/imergefrontend/static/media/bus-stop-icon.20092106.png"
+  },
+  {
+    "revision": "1b05164a0c761b1dacd40b570b8ea265",
+    "url": "/imergefrontend/static/media/church-icon.1b05164a.jpg"
+  },
+  {
+    "revision": "1761904e7c812c1d54f856a48606c708",
+    "url": "/imergefrontend/static/media/embassy-icon.1761904e.png"
+  },
+  {
+    "revision": "1104ea246562867780061aa0c1a75643",
+    "url": "/imergefrontend/static/media/flag-2.1104ea24.jpg"
+  },
+  {
+    "revision": "193ff50a1f86e992eb97de99e81d7256",
+    "url": "/imergefrontend/static/media/grocery-icon.193ff50a.png"
+  },
+  {
+    "revision": "72e3e78e772ee6e61d7ef0d9187312d1",
+    "url": "/imergefrontend/static/media/hospital-icon.72e3e78e.png"
+  },
+  {
+    "revision": "b3bb92e47e49fdb15a31a21a61793e29",
+    "url": "/imergefrontend/static/media/immigration-icon.b3bb92e4.png"
+  },
+  {
+    "revision": "b03668b93d17153bbbf3055f2a667c7e",
+    "url": "/imergefrontend/static/media/kids-2.b03668b9.png"
+  },
+  {
+    "revision": "36d7431729427232f6535f0f5beb0cd1",
+    "url": "/imergefrontend/static/media/mosque-icon.36d74317.png"
+  },
+  {
+    "revision": "d0c1ccd4b4359b3d04e8ee1fe93dc9fd",
+    "url": "/imergefrontend/static/media/movie-2-icon.d0c1ccd4.jpg"
+  },
+  {
+    "revision": "0d2ed85140d7da53ac089a22f916bfcf",
+    "url": "/imergefrontend/static/media/park-icon.0d2ed851.png"
+  },
+  {
+    "revision": "727437e7c0569503e37fe12dce95a75d",
+    "url": "/imergefrontend/static/media/school-icon.727437e7.jpg"
+  },
+  {
+    "revision": "49732e7b623e261d63f7032acb1470ef",
+    "url": "/imergefrontend/static/media/temple-2-icon.49732e7b.jpg"
+  }
+]);
